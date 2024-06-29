237fab61442838c813902a4d6da891463bef6da1
domingo1021 <domingo.backend@gmail.com>
2024-06-29 13:04:24 +0800

    feat: add regex for external api response validation

--- src/modules/hero/hero.validator.ts
+++ src/modules/hero/hero.validator.ts
@@ -4,13 +4,20 @@ import { Hero, HeroProfile } from './dto';
  * A class that contains static utils methods to validate hero data.
  */
 export class HeroDataValidator {
+  static readonly INT_REGEX = /^-?\d+$/;
+  static readonly ALPHABET_REGEX = /^[a-zA-Z ]+$/;
+  static readonly URL_REGEX = /^https?:\/\/[^\s/$.?#].[^\s]*$/;
+
   public static isHero(obj: any): obj is Hero {
     return (
       typeof obj === 'object' &&
       obj !== null &&
       typeof obj.id === 'string' &&
+      HeroDataValidator.INT_REGEX.test(obj.id) &&
       typeof obj.name === 'string' &&
-      typeof obj.image === 'string'
+      HeroDataValidator.ALPHABET_REGEX.test(obj.name) &&
+      typeof obj.image === 'string' &&
+      HeroDataValidator.URL_REGEX.test(obj.image)
     );
   }
 
@@ -19,9 +26,13 @@ export class HeroDataValidator {
       typeof obj === 'object' &&
       obj !== null &&
       typeof obj.str === 'number' &&
+      HeroDataValidator.INT_REGEX.test(`${obj.str}`) &&
       typeof obj.int === 'number' &&
+      HeroDataValidator.INT_REGEX.test(`${obj.int}`) &&
       typeof obj.agi === 'number' &&
-      typeof obj.luk === 'number'
+      HeroDataValidator.INT_REGEX.test(`${obj.agi}`) &&
+      typeof obj.luk === 'number' &&
+      HeroDataValidator.INT_REGEX.test(`${obj.luk}`)
     );
   }
 }
