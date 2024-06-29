4338904dd7b40f372df4dc94ff4b21ed569801a3
domingo1021 <domingo.backend@gmail.com>
2024-06-29 15:42:35 +0800

    fix: fix potential internal server error trigger by nest lifecycle

---
+++ src/cores/middlewares/requestLog.middleware.ts
@@ -0,0 +1,22 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Response, NextFunction } from 'express';
+
+import { InternalRequest } from '#cores/types';
+import { CustomLog } from '#logger/appLog.service';
+
+@Injectable()
+export class RequestLoggerMiddleware implements NestMiddleware {
+  constructor(private readonly logger: CustomLog) {}
+
+  use(req: InternalRequest, _res: Response, next: NextFunction) {
+    const requestId = req.locals.requestId;
+    const { method, url, headers, body } = req;
+
+    this.logger.log(
+      requestId,
+      `Request, ${method}, ${url}, ${JSON.stringify(headers)}, ${JSON.stringify(body)}`,
+    );
+
+    next();
+  }
+}
--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,9 +1,16 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
 
+import { INTERNAL_APIS } from '#cores/constants';
 import { AllExceptionsFilter } from '#cores/exceptions/general.exception';
-import { RequestIdInterceptor } from '#cores/interceptors/requestId.interceptor';
-import { LogInterceptor } from '#cores/interceptors/responseLog.interceptor';
+import { ResponseLoggerInterceptor } from '#cores/interceptors/responseLog.interceptor';
+import { RequestIdMiddleware } from '#cores/middlewares/requestId.middleware';
+import { RequestLoggerMiddleware } from '#cores/middlewares/requestLog.middleware';
 import { AppController } from '#app/app.controller';
 import { HeroModule } from '#hero/hero.module';
 import { HeroController } from '#hero/hero.controller';
@@ -14,10 +21,21 @@ import { LoggerModule } from '#logger/logger.module';
   imports: [LoggerModule, HeroModule],
   controllers: [HeroController, AppController],
   providers: [
-    { provide: APP_INTERCEPTOR, useClass: RequestIdInterceptor },
-    { provide: APP_INTERCEPTOR, useClass: LogInterceptor },
+    { provide: APP_INTERCEPTOR, useClass: ResponseLoggerInterceptor },
     { provide: APP_FILTER, useClass: AllExceptionsFilter },
     CustomLog,
   ],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    const excludedRoutes = INTERNAL_APIS.map((url) => ({
+      path: url,
+      method: RequestMethod.GET,
+    }));
+
+    consumer
+      .apply(RequestIdMiddleware, RequestLoggerMiddleware)
+      .exclude(...excludedRoutes)
+      .forRoutes('*');
+  }
+}
